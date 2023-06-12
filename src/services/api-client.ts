7c40d0b1b44e804338fc22d1f5da7fdb6f455376
Gustavo Miranda <gmiranda90@outlook.com>
2023-06-11 21:46:18 -0300

    Refactoring to fetch games using custom hook

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: ''
+        key: 'fdd3215d09bb4c1f86f6139552c866e6'
     }
 })
