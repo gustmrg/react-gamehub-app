8c8b2e9a897c70065c03b15c04dccdd2b911de65
Gustavo Miranda <gmiranda90@outlook.com>
2023-06-17 20:44:26 -0300

    Reverted back to fetching genres and platform data

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,11 @@
-import platforms from "../data/platforms";
+import useData from "./useData";
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 
 export default usePlatforms;
