8528b63cc8059883619caafdc806ca550ce0c802
Gustavo Miranda <gmiranda90@outlook.com>
2023-06-17 20:39:05 -0300

    fix: platforms not filtering correctly

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,5 @@
 import platforms from "../data/platforms";
 
-const usePlatforms = () => ({ data: platforms, error: null });
+const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
 
 export default usePlatforms;
